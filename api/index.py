70eadab99f00da7edd23a822156f6e030061cd53
Anirban Aditya Halder <nabovtl@gmail.com>
2024-05-30 13:50:31 +0530

    sleep removed

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/', methods=["GET"])
 def home():
-    time.sleep(20)
+    # time.sleep(20)
     return [{'name':"Anirban"}, {'name':"Rahul"}, {'name':"Tanya"}, {'name':"Indresh"}, {'name':"Golu"}], 200
 
 @app.route('/about')
