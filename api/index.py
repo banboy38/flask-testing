b594c62207ce620c55d579eac3c0576182c8e11d
Anirban Aditya Halder <nabovtl@gmail.com>
2024-05-30 13:39:09 +0530

    added timeout

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home():
+    time.sleep(20)
     return [{'name':"Anirban"}, {'name':"Rahul"}, {'name':"Tanya"}, {'name':"Indresh"}, {'name':"Golu"}], 200
 
 @app.route('/about')
